d18ef5d519d592f75ecd1c01e1d1bd59ae1666c8
Rifazul-Islam <rifazul60@gmail.com>
2025-10-24 19:25:48 +0600

    update more

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,16 +2,19 @@
 
 import React from "react";
 import Blogs from "../Blogs/Blogs";
+import Link from "next/link";
 
 export default function Banner() {
   return (
     <div className="lg:mx-26 mt-1.5">
       <div className=" bg-gray-200 rounded-2xl overflow-hidden lg:h-[500px] mt-1">
-        <img
-          src="/gemini-images/gemini-remove.png"
-          alt="Gemini"
-          className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
-        />
+        <Link href="https://gemini.google.com/app">
+          <img
+            src="/gemini-images/gemini-remove.png"
+            alt="Gemini"
+            className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
+          />
+        </Link>
       </div>
       <Blogs />
     </div>
