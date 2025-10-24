6acff8baafef0fb8078ca7351ceacb82ca073ac5
Rifazul-Islam <rifazul60@gmail.com>
2025-10-24 19:42:21 +0600

    remove this link

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,13 +8,11 @@ export default function Banner() {
   return (
     <div className="lg:mx-26 mt-1.5">
       <div className="bg-gray-200 rounded-2xl overflow-hidden lg:h-[500px] mt-1">
-        <Link href="https://gemini.google.com/app">
-          <img
-            src="/gemini-images/gemini-remove.png"
-            alt="Gemini"
-            className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
-          />
-        </Link>
+        <img
+          src="/gemini-images/gemini-remove.png"
+          alt="Gemini"
+          className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
+        />
       </div>
       <div>
         <Link
