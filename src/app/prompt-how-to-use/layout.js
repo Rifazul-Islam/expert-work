fec755b4d35ebebb1cb336de51c137d60c7da8ed
Rifazul-Islam <rifazul60@gmail.com>
2025-11-11 20:59:36 +0600

    update

--- src/app/prompt-how-to-use/layout.js
+++ src/app/prompt-how-to-use/layout.js
@@ -1,12 +1,12 @@
 export const metadata = {
   title: "Gemini ai photo prompt how to use",
   description:
-    "Explore the best Gemini AI photo prompts for how to use — easy to copy and paste! Create stunning, aesthetic, and realistic AI-generated female photos. Perfect for stylish social media and creative projects.",
+    "Explore the best Gemini AI photo prompts for how to use — easy to copy and paste! Create stunning, aesthetic, and realistic AI-generated . Perfect for stylish social media and creative projects.",
 
   openGraph: {
     title: "Gemini ai photo prompt copy paste for how-to-use",
     description:
-      "Explore the best Gemini AI photo prompts for how to use — easy to copy and paste! Create stunning, aesthetic, and realistic AI-generated female photos. Perfect for stylish social media and creative projects.",
+      "Explore the best Gemini AI photo prompts for how to use — easy to copy and paste! Create stunning, aesthetic, and realistic AI-generated . Perfect for stylish social media and creative projects.",
     url: "https://prompt-expert.info/prompt-how-to-use",
     siteName: "Prompt Expert",
 
